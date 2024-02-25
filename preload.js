cb2e681a420e9619757fc7bf2f246b49e879d3b2
Lsnsh <zehexin6@gmail.com>
2024-02-26 05:11:55 +0900

    chore: display project version info

--- preload.js
+++ preload.js
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
+  project: () => process.env.npm_package_version,
   ping: () => ipcRenderer.invoke('ping'),
   // 除函数之外，我们也可以暴露变量
 });

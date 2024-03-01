15e77f9f69391184dfb3a619e885dfbf848a01f2
Lsnsh <zehexin6@gmail.com>
2024-03-01 22:16:15 +0900

    chore: adjust landing page and alert content

--- src/renderer.js
+++ src/renderer.js
@@ -1,13 +1,14 @@
 const information = document.getElementById('info');
-information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`;
+information.innerText = `This application is currently in use Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
 
-information.append([`, 应用版本: v${versions.project()}`]);
+information.append([`, Application (v${versions.application() || '???'})`]);
 
 const func = async () => {
   const response = await window.versions.ping();
   console.log(response); // 打印 'pong'
 
-  window.alert(`response: ${response}`);
+  window.alert(`invoke ping response: ${response}`);
+
   // const message = document.createElement("p")
   // message.innerHTML = `<p>response: ${response}</p>`
   // information.appendChild(message)
